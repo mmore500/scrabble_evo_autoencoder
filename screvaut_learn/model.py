3ba5374aedd747ad8edc39e1ca67717be0017dc8
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-01-26 23:34:05 -0500

    Add model 2

--- screvaut_learn/model.py
+++ screvaut_learn/model.py
@@ -38,3 +38,49 @@ class Model1(nn.Module):
         x = self.linlayer(x.view(x.size(0), -1))
 
         return x
+
+class Model2(nn.Module):
+
+    def __init__(self, channel_counts, kernel_sizes, input_length, dropout=None):
+        super(Model2, self).__init__()
+
+        for k in kernel_sizes:
+            if k % 2 == 0:
+                raise ValueError("kernel size must be odd")
+
+        ios = zip([len(VALID_CHARS)] + channel_counts, channel_counts)
+
+        lays = [
+                lay for (i, o), k in zip(ios, kernel_sizes) for lay in ((nn.Conv1d(i, o, k, padding=k//2), nn.ReLU(), nn.Droput(p=dropout)) if dropout else (nn.Conv1d(i, o, k, padding=k//2), nn.ReLU()))
+            ]
+
+
+        self.cnnlayer = nn.Sequential(*lays)
+
+        self.linlayer = nn.Sequential(
+                nn.Linear(input_length*channel_counts[-1], input_length*channel_counts[-1]),
+                nn.ReLU(),
+                nn.Dropout(p=dropout),
+                nn.Linear(input_length*channel_counts[-1], input_length*channel_counts[-1]),
+                nn.ReLU(),
+                nn.Dropout(p=dropout),
+                nn.Linear(input_length*channel_counts[-1], len(VALID_CHARS))
+            ) if dropout else nn.Sequential(
+                nn.Linear(input_length*channel_counts[-1], input_length*channel_counts[-1]),
+                nn.ReLU(),
+                nn.Linear(input_length*channel_counts[-1], input_length*channel_counts[-1]),
+                nn.ReLU(),
+                nn.Linear(input_length*channel_counts[-1], len(VALID_CHARS))
+            )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d) or isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight)
+
+
+    def forward(self, x):
+
+        x = self.cnnlayer(x)
+        x = self.linlayer(x.view(x.size(0), -1))
+
+        return x
---
+++ script/train2.py
@@ -0,0 +1,49 @@
+import torch
+from torch import nn
+from screvaut_learn.model import Model2
+from screvaut_learn.learn import learn
+from screvaut_learn.dat import DEFAULT_LEARN_PARAMS
+
+import sys
+import json
+
+# cuda as first argument
+cuda = 'true' in sys.argv[1]
+
+print("Cuda set to %r" % cuda)
+
+p = DEFAULT_LEARN_PARAMS
+p['print_every'] = 20
+p['cuda'] = cuda
+p['dropout'] = 0.25
+
+learning_rate = 0.007
+
+print("loading loaders")
+train = torch.load("train_loader.pt")
+test = torch.load("test_loader.pt")
+print("done")
+
+print("making model")
+model = Model2([8000,2000,1000,500], [5,3,3,3], 15)
+print("done")
+
+print("criterion")
+criterion = nn.CrossEntropyLoss()
+
+if p['cuda']:
+    print("moving model to GPU")
+    model = model.cuda()
+    print("moving criterion to GPU")
+    criterion = criterion.cuda()
+
+print("optimizer")
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+print("entering learn")
+model, records = learn(model, train, test, criterion, optimizer, p=p)
+print("leaving learn")
+
+torch.save(model, 'model.pt')
+
+json.dump(records, open('records.json', 'w'))
