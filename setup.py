df981c1df1630f733a913d5131ab152267f688af
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-01-30 10:04:02 -0500

    Bump version: 1.0.7 → 1.0.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='scrabble_evo_autoencoder',
-    version='1.0.7',
+    version='1.0.8',
     description="Experiments using autoencoders to learn evolvable encodings for scrabble strings.",
     author="Matthew Andres Moreno",
     author_email='mmore500@msu.edu',
